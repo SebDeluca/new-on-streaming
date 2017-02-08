08f187b1622155e4139f3380a4400fdafb6eb407
Sebastian DeLuca <sebastianmdeluca@gmail.com>
2017-02-07 22:51:32 -0800

    updated signup post method

--- new_on_streaming.rb
+++ new_on_streaming.rb
@@ -57,15 +57,15 @@ MOVIES= ["The Care Bears Movie (February 1)",
 "Captain Fantastic (February 27)",
 "Havana Motor Club (February 27)"]
 
-get "/" do
-
-
-  erb :main
-end
-
 post "/signup" do
 
   Pony.mail(:to => params[:email], :from => "sd@distribute.com", :subject => "Test Email Subject", :body => "Test Email Body")
 
   redirect "/"
+end
+
+get "/" do
+
+
+  erb :main
 end
